bd2ac3a3b5b3afe55d0f877b4125bdaa13015bb8
vladStar223 <vlad32kolm@gmail.com>
2024-09-06 11:20:41 +0300

    add Testing Route

--- lib/ui/widgets/anime_card.dart
+++ lib/ui/widgets/anime_card.dart
@@ -14,36 +14,63 @@ class AnimeCard extends StatelessWidget{
   Widget build(BuildContext context) {
     // TODO: implement build
     return Card(
-      child: Row(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Flexible(
+    child: Row(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Flexible(
             flex: 4,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: ClipRRect(
-                    borderRadius: BorderRadius.circular(10),
-                    child: Image.network(urlImage)),
-              )),
-          Flexible(
-            flex:3 ,
-              child: Wrap(
-                direction: Axis.horizontal,
-                children: [
-                  Text(name,style: TextStyle(fontSize: 20),),
-                  Text(about,
-                    maxLines: 8,
-                    softWrap: true,
-                    overflow: TextOverflow.ellipsis,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: CachedNetworkImage(
+                  imageUrl: urlImage,
+                  imageBuilder: (context, imageProvider) => Image(
+                    image: imageProvider,
                   ),
+                  placeholder: (context, url) => Icon(Icons.person_2),
+                  errorWidget: (context, url, error) => Icon(Icons.error),
+                ),),
+            )),
+        Flexible(
+            flex:3 ,
+            child: Wrap(
+              direction: Axis.horizontal,
+              children: [
+                Text(name,style: TextStyle(fontSize: 20),),
+                Text(about,
+                  maxLines: 7,
+                  softWrap: true,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                TextButton(onPressed: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (_) {
+                    return AnimeAbout();  }));
+                }, child: Text('Testing')),
+              ],
+            ))
+      ],
+    )
+        );
+    throw UnimplementedError();
+  }
+}
+class AnimeAbout extends StatelessWidget{
+  const AnimeAbout({super.key});
 
-                ],
-              ))
-        ],
-      )
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: Column(
+      children: [TextButton(onPressed: (){
+        Navigator.pop(context);
+      }, child: Text('dsds'))],
+            ),
     );
     throw UnimplementedError();
   }
+
 }
